7d928225432d25ecc6cd44573fb78d7cb03140af
lsteelandt <luc.steelandt@gmail.com>
2023-12-21 08:15:07 +0100

    Update ImageGray16.h

--- components/t547/ImageGray16.h
+++ components/t547/ImageGray16.h
@@ -9,6 +9,8 @@ class ImageGray16 : public image::Image
     public:
         ImageGray16(const uint8_t *data_start, int width, int height,  image::ImageType type);
         void drawInverted(int x, int y, display::Display *display, Color color_on, Color color_off);
-}
+};
+
+
 
 }
