51b71f72563a8e329aea2247ca4945905e294631
lsteelandt <luc.steelandt@gmail.com>
2023-12-21 08:13:22 +0100

    include namespace esphome

--- components/t547/ImageGray16.h
+++ components/t547/ImageGray16.h
@@ -2,10 +2,13 @@
 
 #include "esphome/components/image/image.h"
  
-class ImageGray16 : public esphome::image::Image 
+namespace esphome {
+
+class ImageGray16 : public image::Image 
 {
     public:
-        ImageGray16(const uint8_t *data_start, int width, int height,  esphome::image::ImageType type);
-        void drawInverted(int x, int y, esphome::display::Display *display, esphome::Color color_on, esphome::Color color_off);
+        ImageGray16(const uint8_t *data_start, int width, int height,  image::ImageType type);
+        void drawInverted(int x, int y, display::Display *display, Color color_on, Color color_off);
 }
 
+}
